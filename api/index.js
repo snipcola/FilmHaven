74283c8a26fe58537db2353e14e26d5803da9ca5
Snipcola <snipcola@gmail.com>
2024-02-11 06:35:39 +0000

    re-organize api folder

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-import { initializeEnvironmentVariables } from "./lib/other/env.js";
-import { initializeServer } from "./lib/server.js";
+import { initializeEnvironmentVariables } from "./src/other/env.js";
+import { initializeServer } from "./src/server.js";
 
 initializeEnvironmentVariables();
 const { success, address, error } = await initializeServer();
