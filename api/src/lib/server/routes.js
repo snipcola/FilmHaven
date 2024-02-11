584196f2e8856d021dadc8d8516fe58a03511bec
Snipcola <snipcola@gmail.com>
2024-02-11 22:52:53 +0000

    use post, custom 404 handler

--- api/src/lib/server/routes.js
+++ api/src/lib/server/routes.js
@@ -1,13 +1,17 @@
 import { onRootRequest, onRequest } from "./request.js";
 
 export default function (server) {
-    server.get("/api", onRootRequest);
+    server.post("/api", onRootRequest);
 
-    server.get("/api/:provider/:id/:imdbId?", async function (...args) {
+    server.post("/api/:provider/:id/:imdbId?", async function (...args) {
         return await onRequest("movie", ...args);
     });
 
-    server.get("/api/:provider/:id/:season/:episode/:imdbId?", async function (...args) {
+    server.post("/api/:provider/:id/:season/:episode/:imdbId?", async function (...args) {
         return await onRequest("tv", ...args);
+    });
+
+    server.setNotFoundHandler(function (_, reply) {
+        reply.code(404).send("404 Not Found");
     });
 }
--- site/src/scripts/api/proxy.js
+++ site/src/scripts/api/proxy.js
@@ -1,6 +1,6 @@
 export async function isValidProxy(proxy) {
     try {
-        const response = await fetch(proxy);
+        const response = await fetch(proxy, { method: "POST" });
         const json = await response.json();
 
         return json.success
@@ -13,9 +13,11 @@ export async function isValidProxy(proxy) {
 
 export async function isValidUrl(proxy, provider, info, season, episode) {        
     try {
-        const response = (info.type === "movie")
-            ? await fetch(`${proxy}/${provider}/${info.id}/${info.imdbId}`)
-            : await fetch(`${proxy}/${provider}/${info.id}/${season}/${episode}/${info.imdbId}`);
+        const url = (info.type === "movie")
+            ? `${proxy}/${provider}/${info.id}/${info.imdbId}`
+            : `${proxy}/${provider}/${info.id}/${season}/${episode}/${info.imdbId}`;
+
+        const response = await fetch(url, { method: "POST" });
         const json = await response.json();
 
         return json.success
