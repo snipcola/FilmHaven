c5c410f7b31fc949e3640ac4aa36a1515851a89f
Snipcola <snipcola@gmail.com>
2024-05-09 18:35:15 +0100

    refactor code

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 ## 🎬 - FilmHaven
+
 A simple, customizable, and lightweight frontend to view and interact with movies & shows.
 
 <div align="center">
-    <img src="./assets/showcase.png" width="600"/>
+<img src="./assets/showcase.png" width="600"/>
 </div>
 
 **Accessible here:**
-- <a target="_blank" href="https://fh.snipcola.com">Main</a>
-- <a target="_blank" href="https://film-haven.vercel.app">Mirror</a>
+
+- <a target="\_blank" href="https://fh.snipcola.com">Main</a>
+- <a target="\_blank" href="https://film-haven.vercel.app">Mirror</a>
 
 ## ⌨️ - Shortcuts
+
 - `/` - Focuses on search bar. *Navigates out out of any page or modal open.*
 - `[` - Plays previous episode, *if exists*. *Only works on series modal.*
 - `]` - Plays next episode, *if exists*. *Only works on series modal.*
@@ -20,10 +23,12 @@ A simple, customizable, and lightweight frontend to view and interact with movie
 - `x` - Closes modal. *Only works when modal is open.*
 
 ## 💻 - Compilation
+
 1. Ensure you have `node` and `npm` installed.
 2. Run `npm install`.
 3. Run `npm run build` or `npm run dev` depending on what you need.
 4. Access the site at `site/dist/index.html`.
 
 ## ⚠️ - Disclaimer
+
 All files, such as media, are linked from third-party services, which FilmHaven is not responsible for and has no control over.
--- site/src/scripts/api/proxy.js
+++ site/src/scripts/api/proxy.js
@@ -3,33 +3,33 @@ import { isLocal } from "../functions.js";
 const local = isLocal();
 
 export async function isValidProxy(proxy) {
-    try {
-        const response = await fetch(proxy);
-        const json = await response.json();
+  try {
+    const response = await fetch(proxy);
+    const json = await response.json();
+    const providers =
+      json.success &&
+      json.providers
+        .filter((provider) => (local ? provider.local : true))
+        .map((provider) => provider.base);
 
-        return json.success
-            ? json.providers
-                .filter((provider) => local ? provider.local : true)
-                .map((provider) => provider.base)
-            : false;
-    } catch {
-        return false;
-    }
+    return providers || false;
+  } catch {
+    return false;
+  }
 }
 
-export async function isValidUrl(proxy, provider, info, season, episode) {        
-    try {
-        const url = (info.type === "movie")
-            ? `${proxy}/${provider}/${info.id}/${info.imdbId}`
-            : `${proxy}/${provider}/${info.id}/${season}/${episode}/${info.imdbId}`;
+export async function isValidUrl(proxy, provider, info, season, episode) {
+  try {
+    const url =
+      info.type === "movie"
+        ? `${proxy}/${provider}/${info.id}/${info.imdbId}`
+        : `${proxy}/${provider}/${info.id}/${season}/${episode}/${info.imdbId}`;
 
-        const response = await fetch(url);
-        const json = await response.json();
+    const response = await fetch(url);
+    const json = await response.json();
 
-        return json.success
-            ? json.url
-            : false;
-    } catch {
-        return false;
-    }
+    return json.success ? json.url : false;
+  } catch {
+    return false;
+  }
 }
