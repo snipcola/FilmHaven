1a331d3410863cf56ed25ee168635a9f3cf67c72
Snipcola <snipcola@gmail.com>
2024-05-11 16:50:06 +0100

    make vidsrc.pro default

--- api/src/config.js
+++ api/src/config.js
@@ -1,68 +1,68 @@
 export default {
-    providers: [
-        {
-            base: "vidsrc.to",
-            local: true,
-            url: function (type, params) {
-                switch (type) {
-                    case "movie":
-                        return `https://${this.base}/embed/movie/${params.id}`;
-                    case "tv":
-                        return `https://${this.base}/embed/tv/${params.id}/${params.season}/${params.episode}`;
-                }
-            }
-        },
-        {
-            base: "vidsrc.pro",
-            local: false,
-            url: function (type, params) {
-                switch (type) {
-                    case "movie":
-                        return `https://${this.base}/embed/movie/${params.id}`;
-                    case "tv":
-                        return `https://${this.base}/embed/tv/${params.id}/${params.season}/${params.episode}`;
-                }
-            }
-        },
-        {
-            base: "vidsrc.me",
-            local: true,
-            url: function (type, params) {
-                switch (type) {
-                    case "movie":
-                        return `https://${this.base}/embed/movie?tmdb=${params.id}`;
-                    case "tv":
-                        return `https://${this.base}/embed/tv?tmdb=${params.id}&season=${params.season}&episode=${params.episode}`;
-                }
-            }
-        },
-        {
-            base: "flixon.click",
-            local: true,
-            url: function (type, params) {
-                switch (type) {
-                    case "movie":
-                        return `https://${this.base}/${params.imdbId}`;
-                    case "tv":
-                        return `https://${this.base}/${params.id}-${params.season}-${params.episode}`;
-                }
-            }
+  providers: [
+    {
+      base: "vidsrc.pro",
+      local: false,
+      url: function (type, params) {
+        switch (type) {
+          case "movie":
+            return `https://${this.base}/embed/movie/${params.id}`;
+          case "tv":
+            return `https://${this.base}/embed/tv/${params.id}/${params.season}/${params.episode}`;
         }
+      },
+    },
+    {
+      base: "vidsrc.to",
+      local: true,
+      url: function (type, params) {
+        switch (type) {
+          case "movie":
+            return `https://${this.base}/embed/movie/${params.id}`;
+          case "tv":
+            return `https://${this.base}/embed/tv/${params.id}/${params.season}/${params.episode}`;
+        }
+      },
+    },
+    {
+      base: "vidsrc.me",
+      local: true,
+      url: function (type, params) {
+        switch (type) {
+          case "movie":
+            return `https://${this.base}/embed/movie?tmdb=${params.id}`;
+          case "tv":
+            return `https://${this.base}/embed/tv?tmdb=${params.id}&season=${params.season}&episode=${params.episode}`;
+        }
+      },
+    },
+    {
+      base: "flixon.click",
+      local: true,
+      url: function (type, params) {
+        switch (type) {
+          case "movie":
+            return `https://${this.base}/${params.imdbId}`;
+          case "tv":
+            return `https://${this.base}/${params.id}-${params.season}-${params.episode}`;
+        }
+      },
+    },
+  ],
+  blacklist: {
+    status: [
+      404, // Not Found
+      500, // Internal Server Error
     ],
-    blacklist: {
-        status: [
-            404, // Not Found
-            500 // Internal Server Error
-        ],
-        text: [
-            "not found",
-            "no sources",
-            "no movie found",
-            "no tv show found",
-            "no episode found",
-            "no show found",
-            "media is unavailable", // vidsrc.me
-            "onionplay streaming mirrors" // flixon.click
-        ]
-    }
+    text: [
+      "not found",
+      "no sources",
+      "no movie found",
+      "no tv show found",
+      "no episode found",
+      "no show found",
+      "media is unavailable", // vidsrc.me
+      "onionplay streaming mirrors", // flixon.click
+    ],
+  },
 };
