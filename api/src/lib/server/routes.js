861a7f2b5e23c89d4c161c8129186e16aed7b99a
Snipcola <snipcola@gmail.com>
2024-08-17 13:44:57 +0100

    package.json: update vercel

--- api/src/lib/server/routes.js
+++ api/src/lib/server/routes.js
@@ -10,13 +10,14 @@ async function resolve(type, req) {
   return await onRequest(info);
 }
 
+function notFound(_, reply) {
+  reply.code(404).send("404: Not Found");
+}
+
 export default function (server) {
+  server.setNotFoundHandler(notFound);
   server.get("/api/:id/:imdbId", (...args) => resolve("movie", ...args));
   server.get("/api/:id/:imdbId/:season/:episode", (...args) =>
     resolve("tv", ...args),
-  );
-
-  server.setNotFoundHandler((_, reply) =>
-    reply.code(404).send("404: Not Found"),
   );
 }
